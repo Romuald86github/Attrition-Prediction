c643ba1eb12b197ff9610fd7b28587c1065ddd6c
Romuald86github <123880565+Romuald86github@users.noreply.github.com>
2024-07-02 15:51:01 +0000

    creating scripts

---
+++ src/features/feature_selection.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from mage_ai import data_loader, data_exporter
+
+@data_loader
+def load_data():
+    data = pd.read_csv("data/processed/clean_data.csv")
+    return data
+
+@data_exporter
+def select_features(data, selected_features):
+    selected_data = data[selected_features]
+    selected_data.to_csv("data/processed/selected_data.csv", index=False)
+    return selected_data
+
+if __name__ == "__main__":
+    data = load_data()
+    target_column = "Attrition"
+    selected_features = ['DailyRate', 'DistanceFromHome', 'EnvironmentSatisfaction',
+                         'JobInvolvement', 'JobSatisfaction', 'MaritalStatus', 'NumCompaniesWorked',
+                         'OverTime', 'StockOptionLevel', 'TotalWorkingYears', 'TrainingTimesLastYear',
+                         'WorkLifeBalance', 'YearsAtCompany']
+    select_features(data, selected_features)
+    
