7b5ceac6ad6b4e8f3c4a2711dd00f14e2232a4cf
Romuald86github <romualdtcheutchoua@gmail.com>
2024-07-10 09:18:29 +0100

    updates

--- my-attrition-prediction-project/transformers/select_features.py
+++ my-attrition-prediction-project/transformers/select_features.py
@@ -1,14 +1,37 @@
 import pandas as pd
-from mage_ai.data_preparation.decorators import transformer
 
-@transformer
-def select_features(data, *args, **kwargs):
-    selected_features = kwargs.get('selected_features', ['DailyRate', 'DistanceFromHome', 'EnvironmentSatisfaction',
+
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+def select_number_columns(df: DataFrame) -> DataFrame:
+    return df
+
+
+def features (df: DataFrame) -> DataFrame:
+    return df[['DailyRate', 'DistanceFromHome', 'EnvironmentSatisfaction',
                          'JobInvolvement', 'JobSatisfaction', 'MaritalStatus', 'NumCompaniesWorked',
                          'OverTime', 'StockOptionLevel', 'TotalWorkingYears', 'TrainingTimesLastYear',
-                         'WorkLifeBalance', 'YearsAtCompany'])
-    target_column = kwargs.get('target_column', 'Attrition')
+                         'WorkLifeBalance', 'YearsAtCompany', 'Attrition']]
+
+@transformer
+def select_features(df: DataFrame, *args, **kwargs) -> DataFrame:
+    return features(select_number_columns(df))
+
+
+
+@test
+def test_output(df: DataFrame) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert df is not None, 'The output is undefined'
+    assert isinstance(df, DataFrame), 'The output is not a Pandas DataFrame'
+    assert all(col in df.columns for col in ['DailyRate', 'DistanceFromHome', 'EnvironmentSatisfaction',
+                                            'JobInvolvement', 'JobSatisfaction', 'MaritalStatus', 'NumCompaniesWorked',
+                                            'OverTime', 'StockOptionLevel', 'TotalWorkingYears', 'TrainingTimesLastYear',
+                                            'WorkLifeBalance', 'YearsAtCompany', 'Attrition']), 'Not all expected columns are present'
     
-    selected_data = data[selected_features + [target_column]]
-    selected_data.to_csv("my-attrition-prediction-project/features/selected_data.csv", index=False)
-    return selected_data
