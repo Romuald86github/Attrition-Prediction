d1fd2d151631dd5f96b7f18f88b77801826e837b
Romuald86github <romualdtcheutchoua@gmail.com>
2024-07-10 10:51:23 +0100

    updates

--- my-attrition-prediction-project/transformers/select_features.py
+++ my-attrition-prediction-project/transformers/select_features.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from pandas import DataFrame
 
-
-
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
 if 'test' not in globals():
@@ -11,18 +9,15 @@ if 'test' not in globals():
 def select_number_columns(df: DataFrame) -> DataFrame:
     return df
 
-
-def features (df: DataFrame) -> DataFrame:
-    return df[['DailyRate', 'DistanceFromHome', 'EnvironmentSatisfaction',
-                         'JobInvolvement', 'JobSatisfaction', 'MaritalStatus', 'NumCompaniesWorked',
-                         'OverTime', 'StockOptionLevel', 'TotalWorkingYears', 'TrainingTimesLastYear',
-                         'WorkLifeBalance', 'YearsAtCompany', 'Attrition']]
+def features(df: DataFrame) -> DataFrame:
+    return df.loc[:, ['DailyRate', 'DistanceFromHome', 'EnvironmentSatisfaction',
+                     'JobInvolvement', 'JobSatisfaction', 'MaritalStatus', 'NumCompaniesWorked',
+                     'OverTime', 'StockOptionLevel', 'TotalWorkingYears', 'TrainingTimesLastYear',
+                     'WorkLifeBalance', 'YearsAtCompany', 'Attrition']]
 
 @transformer
 def select_features(df: DataFrame, *args, **kwargs) -> DataFrame:
     return features(select_number_columns(df))
-
-
 
 @test
 def test_output(df: DataFrame) -> None:
@@ -35,4 +30,3 @@ def test_output(df: DataFrame) -> None:
                                             'JobInvolvement', 'JobSatisfaction', 'MaritalStatus', 'NumCompaniesWorked',
                                             'OverTime', 'StockOptionLevel', 'TotalWorkingYears', 'TrainingTimesLastYear',
                                             'WorkLifeBalance', 'YearsAtCompany', 'Attrition']), 'Not all expected columns are present'
-    
