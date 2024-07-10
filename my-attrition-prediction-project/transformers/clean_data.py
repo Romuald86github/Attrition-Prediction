df3fac35c1b7f63012940191a63159d292c734ec
Romuald86github <123880565+Romuald86github@users.noreply.github.com>
2024-07-10 07:02:17 +0000

    updates

--- my-attrition-prediction-project/transformers/clean_data.py
+++ my-attrition-prediction-project/transformers/clean_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pandas import DataFrame
-from mage_ai.data_preparation.decorators import transformer, test
+
 
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
