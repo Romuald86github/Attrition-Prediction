796724a6b5a1d51b0021f83b35f32b1e69e272c2
Romuald86github <romualdtcheutchoua@gmail.com>
2024-07-12 18:10:41 +0100

    updates

--- my-attrition-prediction-project/data_exporters/export_data.py
+++ my-attrition-prediction-project/data_exporters/export_data.py
@@ -1,12 +1,17 @@
 import os
 import pickle
+from mage_ai.io.file import FileIO
 from mage_ai.data_preparation.decorators import data_exporter
+from pandas import DataFrame
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
 
 @data_exporter
-def export_data():
-    # Load the preprocessed data from the file
-    with open('my-attrition-prediction-project/features/preprocessed_data.pkl', 'rb') as f:
-        X_train, X_val, X_test, y_train, y_val, y_test = pickle.load(f)
-
-    # Return the preprocessed data
-    return X_train, X_val, X_test, y_train, y_val, y_test
+def export_data(X_train: DataFrame, X_val: DataFrame, X_test: DataFrame, y_train: DataFrame, y_val: DataFrame, y_test: DataFrame) -> None:
+    """
+    Template for exporting preprocessed data to filesystem as a pickle file.
+    """
+    filepath = 'preprocessed_data.pkl'
+    preprocessed_data = (X_train, X_val, X_test, y_train, y_val, y_test)
+    FileIO().export(preprocessed_data, filepath, file_format='pickle')
