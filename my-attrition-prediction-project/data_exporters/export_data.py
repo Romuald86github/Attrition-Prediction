33aa1b5d3d08472e9f2e341ca6f9dbe77904a1b0
Romuald86github <romualdtcheutchoua@gmail.com>
2024-07-12 18:18:45 +0100

    commit

--- my-attrition-prediction-project/data_exporters/export_data.py
+++ my-attrition-prediction-project/data_exporters/export_data.py
@@ -8,7 +8,7 @@ if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
 @data_exporter
-def export_data_to_file(df: DataFrame, **kwargs) -> None:
+def export_data(df: DataFrame, **kwargs) -> None:
     """
     Template for exporting preprocessed data to filesystem as a pickle file.
     """
